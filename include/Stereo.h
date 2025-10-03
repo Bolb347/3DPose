deff89a4a6b225b9865aba578dd1d6f5971fdd77
Bolb347 <mp2k20@icloud.com>
2025-10-03 14:50:05 -0700

    Timestamp

--- include/Stereo.h
+++ include/Stereo.h
@@ -16,13 +16,13 @@ typedef class StereoSolver {
 
     public:
     cv::Mat m_depthImg;
-    time_t timestamp;
+    long timestamp;
 
     StereoSolver(cv::VideoCapture *cam1stream, cv::VideoCapture *cam2stream, cv::Mat cameraIntrinsics, double baseline) {
         m_cam1stream = cam1stream;
         m_cam2stream = cam2stream;
         m_depthImg = cv::Mat();
-        timestamp = time(nullptr);
+        timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
         m_stereoBM = cv::StereoBM::create(16 * 5, 21);
         m_cameraIntrinsics = cameraIntrinsics;
         m_baseline = baseline;
@@ -30,17 +30,15 @@ typedef class StereoSolver {
 
     ~StereoSolver() {
         delete m_stereoBM;
-        delete m_cam1stream;
-        delete m_cam2stream;
     }
 
     void solve() {
         cv::Mat img1, img2;
         *m_cam1stream >> img1;
         *m_cam2stream >> img2;
-        cv::cvtColor(img1, img1, cv::COLOR_RGB2GRAY);
-        cv::cvtColor(img2, img2, cv::COLOR_RGB2GRAY);
-        timestamp = time(nullptr);
+        cv::cvtColor(img1, img1, cv::COLOR_BGR2GRAY);
+        cv::cvtColor(img2, img2, cv::COLOR_BGR2GRAY);
+        timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
         m_stereoBM->compute(img1, img2, m_depthImg);
         m_depthImg.convertTo(m_depthImg, CV_32F, 1.0 / 16.0);
         cv::Mat mask = m_depthImg > 0.0f;
